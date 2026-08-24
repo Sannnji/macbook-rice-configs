b7552eddcf5332e03227f4bf922d82d23c84ddf0
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-24 16:41:36 -0400

    update telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
 			'nvim-lua/plenary.nvim',
 			'rcarriga/nvim-notify',
 			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+			'nvim-telescope/telescope-ui-select.nvim',
 		},
 		cmd = "Telescope",
 		keys = {
@@ -13,15 +14,25 @@ return {
 			{ '<leader>fr', '<cmd>Telescope oldfiles<cr>',   desc = 'Open Recent' },
 			{ '<leader>fw', '<cmd>Telescope live_grep<cr>',  desc = 'Find word' }
 		},
+		ft = 'mason',
 		config = function()
 			local telescope = require("telescope")
-			telescope.setup({})
+			telescope.setup({
+				extensions = {
+					['ui-select'] = {
+						require('telescope.themes').get_dropdown({
+							-- more options go here if needed
+						})
+					}
+				}
+			})
 
 			-- Sorting extension
 			telescope.load_extension("fzf")
 
 			-- Notification history extension
 			telescope.load_extension("notify")
+			telescope.load_extension("ui-select")
 		end,
 	},
 }
